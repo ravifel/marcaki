7f55c342f113a6d807c42540d5aa9c0918d28ce2
Ravi Silva <ravifelipee@gmail.com>
2022-03-31 17:58:16 -0300

    primeiro commit

---
+++ sobreExames.js
@@ -0,0 +1,41 @@
+var list =
+    [
+        {
+            "titulo": "HEMOGRAMA",
+            "descrição": "Lorem ipsum dolor sit, amet.",
+            "id": "01"
+        }, {
+            "titulo": "GLICEMIA EM JEJUM",
+            "descrição": "Lorem ipsum dolor sit, amet.",
+            "id": "02"
+        }, {
+            "titulo": "COLESTEROL E TRIGLICERÍDEOS",
+            "descrição": "Lorem ipsum dolor sit, amet.",
+            "id": "03"
+        }, {
+            "titulo": "UREIA E CREATINA",
+            "descrição": "Lorem ipsum dolor sit, amet.",
+            "id": "04"
+        }, {
+            "titulo": "TGO (AST) E TGP (ALT)",
+            "descrição": "Lorem ipsum dolor sit, amet.",
+            "id": "05"
+        }, {
+            "titulo": "TSH E T4 LIVRE",
+            "descrição": "Lorem ipsum dolor sit, amet.",
+            "id": "06"
+        }, {
+            "titulo": "ÁCIDO ÚRICO",
+            "descrição": "Lorem ipsum dolor sit, amet.",
+            "id": "07"
+        }, {
+            "titulo": "EXAMES DE URINA",
+            "descrição": "Lorem ipsum dolor sit, amet.",
+            "id": "08"
+        }, {
+            "titulo": "ELETROCARDIOGRAMA",
+            "descrição": "Lorem ipsum dolor sit, amet.",
+            "id": "09"
+        }
+    ]
+
